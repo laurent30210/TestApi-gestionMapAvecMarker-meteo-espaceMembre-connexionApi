27e0913384dcbe84f69ad5776566478420892704
laurent <61387144+laurent30210@users.noreply.github.com>
2021-01-12 19:04:02 +0100

    Update userLocals.js

--- app/middleware/userLocals.js
+++ app/middleware/userLocals.js
@@ -1,7 +1,6 @@
 const userToLocals = (req, res, next) => {
     if (req.session.user) {
         // si on a un user dans la session, c'est qu'il s'est correctement connecté
-        // pour le rendre plus facilement accessible dans nos views, on l'inscrit dans res.locals
         res.locals.user = req.session.user;
     }
 
